4af790a0964182091146f259bb830bb41f5a4172
sei <sei@codenblock.com>
2025-09-02 19:55:14 +0900

    금일 백업

---
+++ js/items.js
@@ -0,0 +1,39 @@
+const items = [
+    {
+        "index":1, 
+        "project":"영라뉴 웹사이트 리뉴얼",
+        "thumbnail" : "/src/youngranu/thumbnail-1",
+        "history" : {
+            "mainText" : "YOUNGRANN",
+            "subText": "WEB RENEWAL",
+            "task": "PUBLISHING",
+            "year" : 2025,
+        },
+        "description" : {
+            "title": "꾸준한 자기 관리를 위한 <br>헬스&이너뷰티 브랜드, 영라뉴",
+            "subTitle": "관리는 쉽고, 콘텐츠는 풍부하게 <br>영라뉴 이커머스 리뉴얼",
+            "introduce": "영라뉴는 차별화된 기술력, 맞춤 개발 생산 솔루션, 높은 마케팅 역량으로 국내에서 높은 가치를 지닌 프리미엄 이너뷰티 브랜드입니다. 카페24 쇼핑몰로 이전 리뉴얼을 시작하면서 상품 노출 전략, 기획전, 맞춤 옵션 기술 등을 추가해 더 좋은 고객 경험, 쉬운 쇼핑과 자체 콘텐츠, 슬로건, 컬러 등 디자인에 다양하게 적용해 영라뉴의 브랜드 가치를 생동감 있게 표현하고자 했습니다.",
+            "client": "큐어라벨",
+            "type": "카페24",
+            "RELAESE": "2025.01.01",
+            "CATEGORY": "쇼핑몰",
+        },
+        "slide" : {
+            1: "/src/youngranu/slide-1.jpg",
+            2: "/src/youngranu/slide-2.jpg",
+            3: {
+              color:"#FFA600",
+              text:"상품 상세 페이지는 가독성을 높인 시원한 레이아웃과 <br> 직관적인 버튼 배치로 상품 탐색과 구매가 더욱 쉬워졌습니다. <br> 할인율과 추가 정보를 한눈에 확인할 수 있으며, 선택한 옵션은 <br> 하단에 바로 추가되어 수량 조절과 삭제가 손쉽게 가능합니다.",
+            },
+            4: "/src/youngranu/slide-4.jpg",
+            5: "/src/youngranu/slide-5.jpg",
+        },
+        "detailImage" : {
+            1: "/src/youngranu/detail-1.jpg",
+            2: "/src/youngranu/detail-2.jpg",
+            3: "/src/youngranu/detail-3.jpg",
+            4: "/src/youngranu/detail-4.jpg",
+            5: "/src/youngranu/detail-5.jpg",
+        }
+    }
+]
